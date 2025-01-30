a291ff7d7b24e56b8f8be8b76f5f67e1c022a528
Stephen Nixon <45946693+arrowtype@users.noreply.github.com>
2025-01-30 11:37:40 -0500

    Update copy-foreground-to-replace-background.py

--- outlines/copy-foreground-to-replace-background.py
+++ outlines/copy-foreground-to-replace-background.py
@@ -1,6 +1,9 @@
 # MenuTitle: Copy foreground to replace background
 __doc__ = """
 	Replace the background with the foreground layer content.
+
+ 	Note: this is very similar to "Path > Selection to Background",
+  	but doesn’t require (or consider) selection.
 """
 
 import math
